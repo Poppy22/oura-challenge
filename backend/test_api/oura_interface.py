d8d8d8dc219dee5db44ee0c61f1fb4e22381a26e
Poppy22 <carmengpopa13@gmail.com>
2019-09-30 21:25:53 +0300

    Added refresh token function

--- backend/test_api/oura_interface.py
+++ backend/test_api/oura_interface.py
@@ -3,17 +3,40 @@ import requests
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 BASE_URL = "https://api.ouraring.com/"
 TYPE_SLEEP = "v1/sleep"
 TYPE_ACTIVITY = "v1/activity"
 TYPE_READINESS = "v1/readiness"
+envrc = "../../.envrc"
+ACCESS_TOKEN_KEY = "ACCESS_TOKEN"
+REFRESH_TOKEN_KEY = "REFRESH_TOKEN"
 
 
-def get_new_refresh_token(refresh_token, client_id, client_secret):
+def refresh_access_token():
     url = BASE_URL + "oauth/token?grant_type=refresh_token&refresh_token="
-    url += refresh_token
-    url += "&client_id=" + client_id + "&client_secret=" + client_secret
-    return requests.post(url)
+    url += str(REFRESH_TOKEN)
+    url += "&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
+
+    response = requests.post(url)
+    if response.status_code != 200:
+        return False  # it did not work
+
+    response = response.content
+
+    # write new access token and refresh token in envrc
+    file_read = open(envrc, 'r')
+    lines = file_read.readlines()
+    file_read.close()
+    file_write = open(envrc, 'w')
+
+    file_write.writelines([item for item in lines[:-2]])
+    file_write.writelines("export " + ACCESS_TOKEN_KEY + '=\"' + response[ACCESS_TOKEN_KEY.lower()] + "\"\n")
+    file_write.writelines("export " + REFRESH_TOKEN_KEY + '=\"' + response[REFRESH_TOKEN_KEY.lower()] + "\"\n")
+    file_write.close()
+
+    return True
 
 
 def get_user_info():
